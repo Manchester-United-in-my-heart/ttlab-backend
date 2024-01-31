c266a6e28a65715061a9ea9fe69b9bc0a981da89
BuiDuongKham <129467586+BuiDuongKham@users.noreply.github.com>
2024-01-31 13:54:45 +0700

    hard code env

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,17 +5,17 @@ import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 import { AuthModule } from './auth/auth.module';
 import { AdminModule } from './admin/admin.module';
-import { ConfigModule } from '@nestjs/config';
+// import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
     UsersModule,
     ProductsModule,
     AuthModule,
     AdminModule,
-    ConfigModule.forRoot({
-      envFilePath: '.env',
-      isGlobal: true,
-    }),
+    // ConfigModule.forRoot({
+    //   envFilePath: '.env',
+    //   isGlobal: true,
+    // }),
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/database.providers.ts
+++ src/database.providers.ts
@@ -1,12 +1,12 @@
 import * as mongoose from 'mongoose';
-import * as process from 'process';
+// import * as process from 'process';
 
 export const databaseProviders = [
   {
     provide: 'DATABASE_CONNECTION',
     useFactory: (): Promise<typeof mongoose> =>
       mongoose.connect(
-        `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@office.9dnkbti.mongodb.net/?retryWrites=true&w=majority`,
+        `mongodb+srv://admin:admin@office.9dnkbti.mongodb.net/?retryWrites=true&w=majority`,
       ),
   },
 ];
