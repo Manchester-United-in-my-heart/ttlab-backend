a832ac007fe9582f5bee70c5103bdc02aa2324e6
BuiDuongKham <129467586+BuiDuongKham@users.noreply.github.com>
2024-01-31 12:20:50 +0700

    done local + env

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,8 +5,18 @@ import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 import { AuthModule } from './auth/auth.module';
 import { AdminModule } from './admin/admin.module';
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [UsersModule, ProductsModule, AuthModule, AdminModule],
+  imports: [
+    UsersModule,
+    ProductsModule,
+    AuthModule,
+    AdminModule,
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
