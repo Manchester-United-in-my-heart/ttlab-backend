fa497577ee705babb60a3815bf8075a58177669a
BuiDuongKham <129467586+BuiDuongKham@users.noreply.github.com>
2024-01-31 16:24:50 +0700

    modularized database

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors(); // enable cors
+  app.enableCors({
+    origin: '*',
+  }); // enable cors
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
